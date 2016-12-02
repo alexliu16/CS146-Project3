f2a19d7c33e25cf21452b469404e6e2dc22ef2b7
alexliu16 <alexliu16@gmail.com>
2016-12-01 21:40:15 -0800

    Fixed perfect maze generator and maze display.

--- Maze.java
+++ Maze.java
@@ -1,4 +1,7 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Stack;
 
 public class Maze {
 	
@@ -12,9 +15,13 @@ public class Maze {
 		//initialize maze with specified size
 		maze = new MazeNode[size][size];
 		for(int row = 0; row < size; row++)
-			for(int col = 0; col < size; col++)
+			for(int col = 0; col < size; col++){
 				maze[row][col] = new MazeNode(row, col);
-
+				if(row == 0 & col == 0)
+					maze[row][col].hasNorthWall = false;
+				if(row == size - 1 & col == size - 1)
+					maze[row][col].hasSouthWall = false;
+			}	
 		generatePerfectMaze();
 	}
 	
@@ -24,42 +31,46 @@ public class Maze {
 		Stack<MazeNode> stack = new Stack<MazeNode>();
 		int totalCells = size * size;
 		MazeNode currentNode = maze[0][0]; //start at upper left hand corner
+		currentNode.setVisited();
 		int visitedCells = 1;
 		//Runs until all cells have been visited
 		while(visitedCells < totalCells){
-			ArrayList<MazeNode> neighbors = getNeighborsWithAllWalls(currentNode);
-			//System.out.println(neighbors.size() + "\n");
+			ArrayList<MazeNode> neighbors = getValidNeighbors(currentNode);
+			//System.out.println(this);
 			if(!neighbors.isEmpty()){
 				//randomly choose an index
 				int index = random.nextInt(neighbors.size());
 				MazeNode node = neighbors.get(index);
-				currentNode.setNeighbor(node);
+				currentNode.knockDownWall(node);
 				stack.push(currentNode);
 				currentNode = node;
+				currentNode.setVisited();
 				visitedCells ++;
+				
 			}
-			else
+			else if(!stack.isEmpty())
 				currentNode = stack.pop();
 		}
 		
 	}
 	
+	
 	//returns an ArrayList of the neighbors that have all its walls intact
-	public ArrayList<MazeNode> getNeighborsWithAllWalls(MazeNode node){
+	public ArrayList<MazeNode> getValidNeighbors(MazeNode node){
 		ArrayList<MazeNode> neighbors = new ArrayList<MazeNode>();
 		int row = node.row;
 		int col = node.col;
 		//top neighbor
-		if(row - 1 >=0 && maze[row - 1][col].allWallsIntact())
+		if(row - 1 >=0 && maze[row - 1][col].allWallsIntact() && !maze[row - 1][col].wasVisited())
 			neighbors.add(maze[row - 1][col]);
 		//bottom neighbor
-		if(row + 1 < size && maze[row + 1][col].allWallsIntact())
+		if(row + 1 < size && maze[row + 1][col].allWallsIntact() && !maze[row + 1][col].wasVisited())
 			neighbors.add(maze[row + 1][col]);
 		//west neighbor
-		if(col - 1 >= 0 && maze[row][col - 1].allWallsIntact())
+		if(col - 1 >= 0 && maze[row][col - 1].allWallsIntact() && !maze[row][col - 1].wasVisited())
 			neighbors.add(maze[row][col - 1]);
 		//east neighbor
-		if(col + 1 < size && maze[row][col + 1].allWallsIntact())
+		if(col + 1 < size && maze[row][col + 1].allWallsIntact() && !maze[row][col + 1].wasVisited())
 			neighbors.add(maze[row][col + 1]);
 		return neighbors;
 		
@@ -67,29 +78,37 @@ public class Maze {
 	
 	@Override
 	public String toString(){
-		String[][] chars = new String[size*2 - 1][size*2 - 1];
+		String[][] chars = new String[size*2 + 1][size*2 + 1];
 		String s = "";
 		for(int row = 0; row < chars.length; row ++){
 			for(int col = 0; col < chars.length; col ++){
-				MazeNode node = maze[row/2][col/2];
-				if(chars[row][col] == null && row % 2 == 0 && col % 2 == 0)
-					chars[row][col] = "+";
-				else if(chars[row][col] == null && row % 2 == 1 && col % 2 == 0)
-					chars[row][col] = "|";
-				else if(chars[row][col] == null && row % 2 == 0)
-					chars[row][col] = "-";
-				else
+				MazeNode node = null;
+				if(row % 2 == 1 && col % 2 == 1)
+					node = maze[row /2][col/2];
+				//represents corners
+				if((row == 0 && col == 1) || (row == size*2 && col == size*2 - 1))
 					chars[row][col] = " ";
-				
-				if(col % 2 ==0 && row % 2 == 0 && row != 0 && node.north != null )
-					chars[row - 1][col] = " ";
-				else if(col % 2 ==0 && row % 2 == 0 && row < size * 2 && node.south != null )
-					chars[row + 1][col] = " ";
-				else if(col % 2 ==0 && row % 2 == 0 && col != 0 && node.west != null )
-					chars[row][col - 1] = " ";
-				else if(col % 2 ==0 && row % 2 == 0 && col <= (size * 2) && node.east != null )
-					chars[row][col + 1] = " ";
-			
+				else if(chars[row][col] == null && row % 2 == 0 && col % 2 == 0)
+					chars[row][col] = "+";
+				//represents top and bottom row
+				else if(row == 0 || row == chars.length - 1)
+					chars[row][col] = "-";
+				//represents leftmost and rightmost column
+				else if(row % 2 == 1 && (col == 0 || col == chars[0].length - 1))
+					chars[row][col] = "|";
+				else if(node != null){
+					if(node.hasWestWall)
+						chars[row][col - 1] = "|";
+					if(node.hasEastWall)
+						chars[row][col + 1] = "|";
+					if(node.hasNorthWall)
+						chars[row - 1][col] = "-";
+					if(node.hasSouthWall)
+						chars[row + 1][col] = "-";
+					chars[row][col] = " ";
+				}
+				else 
+					chars[row][col] = " ";
 			}
 		}
 		for(int row = 0; row < chars.length; row ++){
@@ -105,28 +124,35 @@ public class Maze {
 	 class MazeNode {
 		int row;
 		int col;
-		MazeNode west;
-		MazeNode east;
-		MazeNode north;
-		MazeNode south;
-		boolean visited;
+		boolean hasNorthWall;
+		boolean hasSouthWall;
+		boolean hasEastWall;
+		boolean hasWestWall;
+		boolean wasVisited;
+		ArrayList<MazeNode> neighbors;
 
 		//node references are initially null
 		//null reference represents a wall
 		public MazeNode(int r, int c) {
-			visited = false;
-			west = east = north = south = null;
+			neighbors = new ArrayList<MazeNode>();
+			wasVisited = false;
+			hasWestWall = hasEastWall = hasNorthWall = hasSouthWall = true;
 			row = r;
 			col = c;
+		
 		}
 		
-		public boolean isVisited(){
-			return visited;
+		public void setVisited(){
+			wasVisited = true;
+		}
+		
+		public boolean wasVisited(){
+			return wasVisited;
 		}
 		
 		//true if all walls are null
 		public boolean allWallsIntact(){
-			return west == null && east == null && north == null && south == null;
+			return neighbors.size() == 0;
 		}
 
 		//equal if in the same location 
@@ -144,38 +170,39 @@ public class Maze {
 		}
 		
 		//sets the references of nodes to each other
-		public void setNeighbor(MazeNode node){
+		public void knockDownWall(MazeNode node){
 			//node is westNeighbor
 			if(node.row == row && node.col == col - 1){
-				west = node;
-				node.east = this;
+				hasWestWall = false;
+				node.hasEastWall = false;
+				neighbors.add(node);
+				node.neighbors.add(this);
 			}
 			else if(node.row == row && node.col == col + 1){
-				east = node;
-				node.west = this;
+				hasEastWall = false;
+				node.hasWestWall = false;
+				neighbors.add(node);
+				node.neighbors.add(this);
 			}
 			else if(node.col == col && node.row == row - 1){
-				north = node;
-				node.south = this;
+				hasNorthWall = false;
+				node.hasSouthWall = false;
+				neighbors.add(node);
+				node.neighbors.add(this);
 			}
 			else if(node.col == col && node.row == row + 1){
-				south = node;
-				node.north = this;
+				hasSouthWall = false;
+				node.hasNorthWall = false;
+				neighbors.add(node);
+				node.neighbors.add(this);
 			}
 		}
 	}
 	
 	public static void main(String[] args){
-		Maze maze = new Maze(4);
-		//commented out code tests the toString method
-		/*maze.maze[0][0].east = maze.maze[0][1];
-		maze.maze[0][1].south = maze.maze[1][1];
-		maze.maze[1][1].east = maze.maze[1][2];
-		maze.maze[2][1].east = maze.maze[2][2];
-		maze.maze[2][2].south = maze.maze[2][3];
-		maze.maze[2][3].south = maze.maze[3][3];*/
+		Maze maze = new Maze(5);
 		System.out.println(maze);
-		
+	
 	}
 
 }
